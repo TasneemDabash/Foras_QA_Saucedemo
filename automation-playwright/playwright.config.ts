da2765037ad867dccf9e2a0cccc8e334449f84a8
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 10:04:15 +0300

    Update Playwright File

--- automation-playwright/playwright.config.ts
+++ automation-playwright/playwright.config.ts
@@ -1,2 +1,12 @@
-import { defineConfig } from '@playwright/test';
-export default defineConfig({});
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    baseURL: 'https://www.saucedemo.com/v1',
+    viewport: { width: 1366, height: 768 }
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } }
+  ]
+});
