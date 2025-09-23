bf4316eb0d8996f1b378a6082e82bcc98b4dbe0a
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 10:49:54 +0300

    Update Cypress/02

--- automation/cypress/e2e/02_cart_checkout.cy.js
+++ automation/cypress/e2e/02_cart_checkout.cy.js
@@ -1,1 +1,19 @@
-describe('Cart & Checkout', () => { it('should checkout', () => {}); });
+describe('Cart & Checkout Flow', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+  });
+
+  it('Add product to cart and checkout with missing postal code', () => {
+    cy.contains('Add to cart').first().click();
+    cy.get('.shopping_cart_link').click();
+    cy.get('.checkout_button').click();
+    cy.get('#first-name').type('John');
+    cy.get('#last-name').type('Doe');
+    cy.get('#postal-code').clear();
+    cy.get('#continue').click();
+    cy.get('[data-test="error"]').should('contain', 'Postal Code is required');
+  });
+});
