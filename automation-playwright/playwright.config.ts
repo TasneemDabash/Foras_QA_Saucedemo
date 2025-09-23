edc3200ddfcd746e5efd9c1de731059883df01c2
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 10:54:28 +0300

    Update Playwright Config

--- automation-playwright/playwright.config.ts
+++ automation-playwright/playwright.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  testDir: 'tests',
+  timeout: 30 * 1000,
+  expect: { timeout: 5000 },
+  retries: process.env.CI ? 1 : 0,
+  reporter: [['list']],
   use: {
-    baseURL: 'https://www.saucedemo.com/v1',
-    viewport: { width: 1366, height: 768 }
+    baseURL: 'https://www.saucedemo.com/v1/',
+    headless: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } }
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ]
 });
