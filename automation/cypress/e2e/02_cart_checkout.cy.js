b4b34721620948432a3e9f53f98408a9b1b3558b
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 12:35:17 +0300

    Update 02_cart_checkout.cy.js

--- automation/cypress/e2e/02_cart_checkout.cy.js
+++ automation/cypress/e2e/02_cart_checkout.cy.js
@@ -4,16 +4,35 @@ describe('Cart & Checkout Flow', () => {
     cy.get('#user-name').type('standard_user');
     cy.get('#password').type('secret_sauce');
     cy.get('#login-button').click();
+
+    cy.url().should('include', 'inventory.html');
+    cy.get('.inventory_list').should('be.visible');
   });
 
   it('Add product to cart and checkout with missing postal code', () => {
-    cy.contains('Add to cart').first().click();
+    cy.contains('button', /add to cart/i).first().click();
+
     cy.get('.shopping_cart_link').click();
-    cy.get('.checkout_button').click();
-    cy.get('#first-name').type('John');
-    cy.get('#last-name').type('Doe');
-    cy.get('#postal-code').clear();
-    cy.get('#continue').click();
-    cy.get('[data-test="error"]').should('contain', 'Postal Code is required');
+    cy.location('pathname').should('match', /\/(v1\/)?cart\.html$/);
+
+    cy.get('.checkout_button').should('be.visible').click();
+    cy.location('pathname').should('match', /\/(v1\/)?checkout-step-one\.html$/);
+
+    cy.get('#checkout_info_container').should('be.visible');
+    cy.get('#first-name').should('be.visible').type('John');
+    cy.get('#last-name').should('be.visible').type('Doe');
+    cy.get('#postal-code').should('be.visible');
+
+    cy.get('#checkout_info_container form')
+      .find('#continue, [data-test="continue"], input[type="submit"], input.btn_primary, input.cart_button')
+      .filter(':visible')
+      .first()
+      .scrollIntoView()
+      .should('be.enabled')
+      .click({ force: true });
+
+    cy.get('[data-test="error"]', { timeout: 10000 })
+      .should('be.visible')
+      .and('contain.text', 'Postal Code is required');
   });
 });
