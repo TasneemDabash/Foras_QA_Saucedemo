2ddd439fed5372059921845d3d71d3fd89cc4db9
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 12:22:31 +0300

    Update 04_logout.cy.js

--- automation/cypress/e2e/04_logout.cy.js
+++ automation/cypress/e2e/04_logout.cy.js
@@ -4,11 +4,24 @@ describe('Logout Test', () => {
     cy.get('#user-name').type('standard_user');
     cy.get('#password').type('secret_sauce');
     cy.get('#login-button').click();
+
+    cy.url().should('include', 'inventory.html');
+    cy.get('.inventory_list').should('be.visible');
   });
 
   it('Logs out successfully', () => {
-    cy.get('#react-burger-menu-btn').click();
-    cy.contains('Logout').click();
-    cy.url().should('eq', 'https://www.saucedemo.com/v1/');
+    cy.get('body').then($body => {
+      if ($body.find('#react-burger-menu-btn').length) {
+        cy.get('#react-burger-menu-btn').click();
+      } else {
+        cy.contains('button', /open menu/i).click();
+      }
+    });
+    cy.get('#logout_sidebar_link', { timeout: 10000 })
+      .should('be.visible')
+      .click();
+    cy.location('pathname', { timeout: 10000 }).should('match', /^\/(v1\/(index\.html)?)?$/);
+
+    cy.get('#login-button').should('be.visible');
   });
 });
