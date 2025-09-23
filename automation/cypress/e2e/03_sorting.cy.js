70699f899b9f5af937ddf6967f325562116e0723
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 10:50:32 +0300

    Update Cypress/03

--- automation/cypress/e2e/03_sorting.cy.js
+++ automation/cypress/e2e/03_sorting.cy.js
@@ -1,1 +1,17 @@
-describe('Sorting', () => { it('should sort', () => {}); });
+describe('Sorting Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+  });
+
+  it('Sort products Z to A', () => {
+    cy.get('.product_sort_container').select('za');
+    cy.get('.inventory_item_name').then(items => {
+      const names = [...items].map(el => el.innerText);
+      const sorted = [...names].sort().reverse();
+      expect(names).to.deep.equal(sorted);
+    });
+  });
+});
