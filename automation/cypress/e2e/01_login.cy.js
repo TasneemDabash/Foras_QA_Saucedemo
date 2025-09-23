cb0555142078ac275b3e25a3af368dd6b6171b88
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2025-09-23 10:49:27 +0300

    Update 01_login.cy.js

--- automation/cypress/e2e/01_login.cy.js
+++ automation/cypress/e2e/01_login.cy.js
@@ -1,1 +1,17 @@
-describe('Login', () => { it('should login', () => {}); }); 
+describe('Login Tests', () => {
+  it('Valid login with standard_user', () => {
+    cy.visit('/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.url().should('include', 'inventory.html');
+  });
+
+  it('Locked_out_user should see error message', () => {
+    cy.visit('/');
+    cy.get('#user-name').type('locked_out_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get('[data-test="error"]').should('be.visible');
+  });
+});
